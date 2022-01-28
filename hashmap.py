79a6d09520049dfe64e45650fcfaa0236d92122d
arnav sharma <arnavjune2@gmail.com>
2022-01-28 20:40:51 +0530

    first commit

---
+++ hashmap.py
@@ -0,0 +1,8 @@
+"""
+
+hashmap isuse to store key and value pairs
+dict[key]---> returns value
+dict[new_key]=new_value
+key in dict---->boolean(i.e if key is present in dict then True else False)
+
+"""
