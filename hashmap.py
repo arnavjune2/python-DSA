0166f5679c6ca14d64f2607d0738a71258d23850
arnav sharma <arnavjune2@gmail.com>
2022-01-28 22:22:37 +0530

    hash commit

--- hashmap.py
+++ hashmap.py
@@ -6,3 +6,15 @@ dict[new_key]=new_value
 key in dict---->boolean(i.e if key is present in dict then True else False)
 
 """
+dict = {1001:["arnav", 89], 1002:["ram", 79], 1002:["shamm", 89]}
+print(dict)
+
+#adding into hashmap
+dict[1003] = ["raju", 56]
+
+#if you want to print( dict[990]), then it will produce an KeyError so instead we can use dict.get(990)
+print(dict.get(990))
+
+#iterating through the hashmap
+for key, value in dict.items():
+    print(key," ",value)
--- queues.py
+++ queues.py
@@ -5,10 +5,15 @@ queues
 queue = ["p1", "p2", "p3", "p4", "p5"]
 
 #enequeue - enter
-
 queue.append("p6")
 print("the que is", queue)
 
-#if it is given how many peope left the queue
+# if it is given how many peope left the queue
 m = int(input("Enter the number of people left the queue:: "))
 print(queue[m:])
+
+# enqueue
+for i in range(m):
+    queue.pop(0)
+
+print(f"queue after {queue}")
