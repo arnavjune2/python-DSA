495ef96152054a79f1f7809efe90e0edf7c041b0
Arnav Sharma <arnavjune2@gmail.com>
2022-08-15 13:35:53 +0530

    generate functinn in linked+list.py

--- linked+list.py
+++ linked+list.py
@@ -28,7 +28,24 @@ class LinkedList:
 
     # Function to insert a new node at the end
     def append(self, new_node):
-        pass 
+        current = self.head
+        while(current.next):
+            current = current.next
+        current.next = new_node
+
+    #Function to generate a linkedList from a array
+    def generate(self, arr):
+        curr = self.head
+        for i in range(len(arr)):
+            #print(1)
+            if curr == None:
+                #print(2)
+                self.head = Node(arr[i])
+                curr = self.head
+            else:    
+                #print(3)
+                curr.next = Node(arr[i])
+                curr = curr.next
 
 #just initaiting a singly linked list    
 if __name__=="__main__":
